b6aa42c0be38217ccbab0322d0083b7a85c9ccb4
krmanik <12841290+krmanik@users.noreply.github.com>
2022-05-30 17:51:13 +0800

    chmod

--- tools/update-js-libs.sh
+++ tools/update-js-libs.sh
@@ -8,20 +8,20 @@ curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.10.1/bazelis
 chmod +x bazel && sudo mv bazel /usr/local/bin/
 
 # clone Anki
-git clone https://github.com/ankitects/anki ~/tmp/anki
+git clone https://github.com/ankitects/anki ~/anki
 
 # build for mathjax and jquery
-cd ~/tmp/anki/qt/aqt/data/web/js/vendor; bazel build vendor
+cd ~/anki/qt/aqt/data/web/js/vendor; bazel build vendor
 
 # change dir to AnkiDroid
 cd /home/runner/work/Anki-Android-Copy/Anki-Android-Copy
 
 # copy latest jquery to assets dir
-cp ~/tmp/anki/.bazel/bin/qt/aqt/data/web/js/vendor/jquery.min.js AnkiDroid/src/main/assets/jquery.min.js
+cp ~/anki/.bazel/bin/qt/aqt/data/web/js/vendor/jquery.min.js AnkiDroid/src/main/assets/jquery.min.js
 
 # remove old mathjax file
 rm -rf AnkiDroid/src/main/assets/mathjax
 mkdir AnkiDroid/src/main/assets/mathjax
 
 # copy latest mathjax to assets dir
-cp -r ~/tmp/anki/.bazel/bin/qt/aqt/data/web/js/vendor/mathjax AnkiDroid/src/main/assets/
+cp -r ~/anki/.bazel/bin/qt/aqt/data/web/js/vendor/mathjax AnkiDroid/src/main/assets/
